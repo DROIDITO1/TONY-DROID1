3b8376be6ff82a2f942fd5bed5eab6d4e6530297
DROIDITO1 <140969913+DROIDITO1@users.noreply.github.com>
2023-09-21 21:57:47 -0600

    Actualizar _ranks.js

--- plugins/_ranks.js
+++ plugins/_ranks.js
@@ -10,10 +10,10 @@ global.rpg = {
       { name: "Semi-medio", level: 8 }, { name: "Aprendiendo", level: 12 }, 
       { name: "Master", level: 16 }, { name: "Guardian", level: 20 }, 
       { name: "Super-master", level: 24 }, { name: "Hero", level: 28 }, 
-      { name: "Semi-nitro", level: 32 }, { name: "Nitro", level: 36 },
-      { name: "Semi-turbo", level: 48 }, { name: "turbo", level: 52 }, 
-      { name: "Super-turbo", level: 56 }, { name: "Super-turbo-nitro", level: 60 }, 
-      { name: "TURBO-NITRO", level: 100 }
+      { name: "Semi-droid", level: 32 }, { name: "Nitro", level: 36 },
+      { name: "Semi-droid", level: 48 }, { name: "turbo", level: 52 }, 
+      { name: "Super-droid", level: 56 }, { name: "Super-turbo-nitro", level: 60 }, 
+      { name: "DIOS-DROID", level: 100 }
     ];
 
     return role.reverse().find(role => level >= role.level)
